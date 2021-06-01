5a34ef101425f7e1234f35dd276ac995a98532c2
JC <jeremyrcastellano@gmail.com>
2021-06-01 13:48:13 -0400

    test router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import { Router, Switch, Route } from 'react-router-dom'
 import { Home } from './Components/Home'
 
 function App() {
