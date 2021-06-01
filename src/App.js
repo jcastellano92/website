eaf63a79aa95168116ddb2adb85aa7fefded0b32
JC <jeremyrcastellano@gmail.com>
2021-06-01 13:49:23 -0400

    test updating to browser router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Home } from './Components/Home'
 
 function App() {
