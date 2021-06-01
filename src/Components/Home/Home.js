a08580a63ca5f40239cbed7cc908a15b191c1bf7
JC <jeremyrcastellano@gmail.com>
2021-06-01 13:46:36 -0400

    update to add router

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,23 @@
+import './Home.css';
+import logo from '../../logo.svg';
+
+export const Home = () => {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <p>
+          Edit <code>src/Home.js</code> and save to reload.
+        </p>
+        <a
+          className="Home-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
